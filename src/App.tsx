4f7885ca2af891734cc68e6918c604563a4dcdb8
persadian <34051185+arishma108@users.noreply.github.com>
2025-02-14 16:06:40 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -229,7 +229,7 @@ function App() {
           <div className="max-w-6xl mx-auto px-4">
             <div className="flex justify-between items-center h-16">
               <Link to="/" className="flex items-center gap-2">
-               /* Blocks className="h-8 w-8 text-blue-500" */
+               
                 <img src="https://avatars.githubusercontent.com/u/62150946?s=200&v=4" alt="DARJYO Logo" className="h-8 w-auto"/>
                  <span className="font-bold text-xl">Braille Bricks</span>
               </Link>
